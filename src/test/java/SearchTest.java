507af5cdb57d41fd2617dc6e10f850541bd101d3
Syrym <cloudkazakhstan@gmail.com>
2023-01-10 23:28:27 +0600

    second change

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.net.PortProber.random;
 
 public class SearchTest {
     @Test
@@ -12,5 +16,8 @@ public class SearchTest {
        open("https://www.google.kz/");
         $("[name=q]").setValue("instagram").pressEnter();
         $("[id=\"center_col\"]").shouldHave(text("https://www.instagram.com"));
+    }
+
+    void test(){
     }
 }
